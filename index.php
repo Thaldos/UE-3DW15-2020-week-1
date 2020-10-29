d5d1b0e815f21b4712c115d83168787fefb80cd6
Jonathan Lesueur <jonathan.lesueur.tic@gmail.com>
2020-10-29 20:48:17 +0100

    second-branch: modification de l'index

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php 
-echo 'bonjour';
+
+$_datas = array('lastname' => 'Lesueur', 'age' => 25);
+$_datas['firstname'] = array('Jonathan','Christian','Georges');
+
+?>
+<table>
+    <tbody>
+        <tr>
+            <th>Nom :</th>
+            <td><?= $_datas['lastname'] ?></td>
+        </tr>
+        <tr>
+            <th>Prénom(s) :</th>
+            <td><?= implode(', ', $_datas['firstname']) ?></td>
+        </tr>
+        <tr>
+            <th>Âge :</th>
+            <td><?= $_datas['age'] ?></td>
+        </tr>
+    </tbody>
+</table>
